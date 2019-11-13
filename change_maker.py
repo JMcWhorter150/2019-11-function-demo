2cd51b5ca67d04615b8726836af065afa23e8312
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-11-13 16:21:18 -0500

    Adds change to total converter and fixes change converter

--- change_maker.py
+++ change_maker.py
@@ -9,35 +9,27 @@
 bills = [1, 5, 10, 20, 50, 100]
 coins = [1, 5, 10, 25]
 def convert_dollars(dollars):
-    # print(dollars)
     bill_count = []
     for bill in range(len(bills) - 1, -1, -1): # -1 from bill length since computer starts at 0, goes until -1 since I want to include 0, and decrements so that we start big and get to small
-        # print(bills[bill])
         count = 0
         while dollars >= bills[bill]:
             dollars -= bills[bill]
             count += 1
         bill_count.append(count)
-    # print(bill_count)
     bill_count.reverse()
     bill_count = tuple(bill_count)
-    # print(bill_count)
     return bill_count
 
 def convert_coins(cents):
-    # print(cents)
     coin_count = []
     for coin in range(len(coins) - 1, -1, -1): # -1 from cent length since computer starts at 0, goes until -1 since I want to include 0, and decrements so that we start big and get to small
-        # print(coins[coin])
         count = 0
         while cents >= coins[coin]:
             cents -= coins[coin]
             count += 1
         coin_count.append(count)
-    # print(coin_count)
     coin_count.reverse()
     coin_count = tuple(coin_count)
-    # print(coin_count)
     return coin_count
 
 def dollar_split(total):
@@ -45,55 +37,46 @@ def dollar_split(total):
     return int(dollars)
 
 def cent_split(total):
-    cents = int((total - dollar_split(total)) * 100)
+    cents = int(round(total - int(total), 2) * 100)
+    print(cents)
     return cents
 
 def make_change(total_charge, payment):
     change = payment - total_charge
+    print(change)
     dollars = dollar_split(change)
+    print(dollars)
     cents = cent_split(change)
+    print(cents)
     total = (convert_dollars(dollars), convert_coins(cents))
     return total
 
-
-# convert_coins(175)
-# convert_dollars(150)
-
-# print(dollar_split(173.31))
-# print(cent_split(173.31))
-
 user_bill = float(input("Give me the bill total you want to split up. "))
 user_paid = float(input("Give me the amount that you paid. "))
-print(make_change(user_bill, user_paid))
+change = make_change(user_bill, user_paid)
+print(change)
 
+def find_dollars(dollars_tuple):
+    sum = 0
+    for i in range(len(dollars_tuple)):
+        sum += dollars_tuple[i] * bills[i]
+    return sum
 
+def find_coins(coins_tuple):
+    sum = 0
+    for i in range(len(coins_tuple)):
+        sum += coins_tuple[i] * coins[i]
+    sum /= 100
+    return sum
 
+def value_of_change(dollar_coin_tuple):
+    total = 0
+    dollars, coins = dollar_coin_tuple
+    print(dollars)
+    print(coins)
+    print(find_dollars(dollars))
+    print(find_coins(coins))
+    total += find_dollars(dollars) + find_coins(coins)
+    return total
 
-
-
-
-
-
-
-
-
-
-#     reverse_bills = []
-#     # reverse_bills.reverse()
-#     # print(reverse_bills)
-#     for bill in range(len(bills), 0, -1):
-#         count = 0
-#         print(bills[bill])
-#         while dollars > bills[bill]:
-#             count += 1
-#             dollars -= bills[bill]
-#             print(count)
-#         reverse_bills.append(count)
-#     # reverse_bills = reverse_bills.reverse()
-#     # total_bills = tuple(reverse_bills)
-#     # return total_bills
-#     return reverse_bills
-# print(convert_dollars(400))
-    
-    
-
+print(value_of_change(change))
